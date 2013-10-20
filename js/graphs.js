a97d305e88b304c89f9f0abc05e62e6095ad8fc5
velazqua <alx.velazquez@gmail.com>
2013-10-20 01:32:28 -0400

    fak this shit

--- js/graphs.js
+++ js/graphs.js
@@ -1,3 +1,3 @@
 $(function () {
-  $.plot($("#graph-placeholder"), [ [[0, 0], [1, 1]] ], { yaxis: { max: 1 } });
+  $.plot($("#graph-placeholder"), [ [[0, 0], [1, 1], [0.25, 0.75]] ], { yaxis: { max: 1 }, xaxis : {autoscaleMargin: 0.5 } });
 });
